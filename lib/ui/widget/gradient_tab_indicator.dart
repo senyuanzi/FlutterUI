6db4b12f3e0fbd7de79f21e5afb74d3041d66335
senyuanzi <senyuanzi@outlook.com>
2022-01-31 12:02:23 +0800

    text

---
+++ lib/ui/widget/gradient_tab_indicator.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:the_flutter_ui/extension/scope_ex.dart';
+
+class GradientTabIndicator extends Decoration {
+  const GradientTabIndicator({
+    this.borderSide = const BorderSide(width: 2.0, color: Colors.white),
+    this.insets = EdgeInsets.zero,
+    this.gradientColor,
+  });
+
+  final BorderSide borderSide;
+
+  final EdgeInsetsGeometry insets;
+  final List<Color>? gradientColor;
+
+  @override
+  Decoration? lerpFrom(Decoration? a, double t) {
+    if (a is UnderlineTabIndicator) {
+      return GradientTabIndicator(
+        borderSide: BorderSide.lerp(a.borderSide, borderSide, t),
+        insets: EdgeInsetsGeometry.lerp(a.insets, insets, t)!,
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  Decoration? lerpTo(Decoration? b, double t) {
+    if (b is UnderlineTabIndicator) {
+      return UnderlineTabIndicator(
+        borderSide: BorderSide.lerp(borderSide, b.borderSide, t),
+        insets: EdgeInsetsGeometry.lerp(insets, b.insets, t)!,
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _UnderlinePainter(this, onChanged);
+  }
+
+  Rect _indicatorRectFor(Rect rect, TextDirection textDirection) {
+    final Rect indicator = insets.resolve(textDirection).deflateRect(rect);
+    return Rect.fromLTWH(
+      indicator.left,
+      indicator.bottom - borderSide.width,
+      indicator.width,
+      borderSide.width,
+    );
+  }
+
+  @override
+  Path getClipPath(Rect rect, TextDirection textDirection) {
+    return Path()..addRect(_indicatorRectFor(rect, textDirection));
+  }
+}
+
+class _UnderlinePainter extends BoxPainter {
+  _UnderlinePainter(this.decoration, VoidCallback? onChanged)
+      : super(onChanged);
+
+  final GradientTabIndicator decoration;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    assert(configuration.size != null);
+    final Rect rect = offset & configuration.size!;
+    final TextDirection textDirection = configuration.textDirection!;
+    final Rect indicator = decoration
+        ._indicatorRectFor(rect, textDirection)
+        .deflate(decoration.borderSide.width / 2.0);
+    final Paint paint = decoration.borderSide.toPaint()
+      ..strokeCap = StrokeCap.square
+      ..shader = decoration.gradientColor?.let((it) =>
+          LinearGradient(colors: decoration.gradientColor!)
+              .createShader(indicator));
+
+    canvas.drawLine(indicator.bottomLeft, indicator.bottomRight, paint);
+  }
+}
